701402301304364dfc1ef938c791d80232216a66
Holdio <holden.d.clark@gmail.com>
2022-10-14 23:26:40 +0000

    Small Change

--- script.js
+++ script.js
@@ -1,41 +1,25 @@
 document.getElementById("Generator").addEventListener("click", function(event) {
+    event.preventDefault()
 
-    const url = "https://random-words-api.vercel.app/word/";
+    const url = "https://api.imgflip.com/get_memes";
     fetch(url)
         .then(function(response) {
             return response.json();
         }).then(function(json) {
-
+            let results = "";
+            let num = Math.floor(Math.random() * json.data.memes.length);
+            results += "<img src=" + json.data.memes[num].url + ">";
+            document.getElementById("image").innerHTML = results;
         });
-
-    // const ameClient = require("amethyste-api")
-    // const ameApi = new ameClient("token")
-
-    // ameApi.image("wallpaper", {
-    //     "nsfw": "false"
-    // }).then(image => {
-    //     console.log(image)
-    // }).catch(err => {
-    //     throw err;
-    // });
-
-    // // const url =
-    // fetch(url)
-    //     .then(function(response) {
-    //         return response.json();
-    //     }).then(function(json) {
-
-    //     });
-
-    const deepai = require('deepai'); // OR include deepai.min.js as a script tag in your HTML
-
-    deepai.setApiKey('3211ccf8-a7c2-4ad6-bc76-f81afe481c63');
-
-    (async function() {
-        var resp = await deepai.callStandardApi("text2img", {
-            text: "YOUR_TEXT_HERE",
+   
+   const url2 = "https://api.quotable.io/random";
+    fetch(url2)
+        .then(function(response) {
+            return response.json();
+        }).then(function(json) {
+            let results = "";
+            results += json.content;
+            document.getElementById("quote").innerHTML = results;
         });
-        console.log(resp);
-    })()
-
+   
 });
